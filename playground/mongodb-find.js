cda2d0cc59abec8ecc7ca623ea1369964746e8b3
Brian Pugh <brian@jaspin.com>
2019-02-14 12:46:00 -0500

    init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,30 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('connected to MongoDB server');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5c657cd14e95878c9e074e42')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 4));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({name:'Brian'}).toArray().then((docs) => {
+        console.log(JSON.stringify(docs, undefined, 4));
+    })
+
+    //db.close();
+});
